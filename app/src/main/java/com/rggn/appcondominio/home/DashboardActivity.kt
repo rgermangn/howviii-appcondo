87f1de9622c27617d6e4c21a42643d4ee867aa55
rgermangn <rgermangn@gmail.com>
2025-11-18 11:36:02 -0300

    INFO-NAV
    Signed-off-by: rgermangn <rgermangn@gmail.com>

--- app/src/main/java/com/rggn/appcondominio/home/DashboardActivity.kt
+++ app/src/main/java/com/rggn/appcondominio/home/DashboardActivity.kt
@@ -2,11 +2,13 @@ package com.rggn.appcondominio.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.rggn.appcondominio.R
-import com.rggn.appcondominio.reservation.CalendarActivity // NOVO IMPORT: Para navegar para a tela do calendário
+import com.rggn.appcondominio.info.InformationActivity
+import com.rggn.appcondominio.reservation.CalendarActivity
 
 class DashboardActivity : AppCompatActivity() {
 
@@ -19,13 +21,16 @@ class DashboardActivity : AppCompatActivity() {
 
         setupRecyclerView()
         observeViewModel()
+
+        val infoButton = findViewById<Button>(R.id.btn_information)
+        infoButton.setOnClickListener {
+            val intent = Intent(this, InformationActivity::class.java)
+            startActivity(intent)
+        }
     }
 
-    // Função de navegação para a tela de reserva
     private fun navigateToReservation(areaId: Int) {
-        // MUDANÇA: Navega para a CalendarActivity
         val intent = Intent(this, CalendarActivity::class.java).apply {
-            // CORRIGIDO: Usa a constante definida na CalendarActivity
             putExtra(CalendarActivity.EXTRA_AREA_ID, areaId)
         }
         startActivity(intent)
@@ -33,12 +38,10 @@ class DashboardActivity : AppCompatActivity() {
 
 
     private fun setupRecyclerView() {
-        // Inicializa o Adapter passando o lambda de clique (NOVO CÓDIGO)
         adapter = CommonAreaAdapter(onAreaClick = { areaId ->
             navigateToReservation(areaId)
         })
 
-        // Configura o RecyclerView
         val recyclerView = findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.areas_recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
